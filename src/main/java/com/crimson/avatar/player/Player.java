333cc021a99d04437a40824f5950ff7667d7f9ab
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2025-02-17 19:33:56 -0300

    Player vars

--- src/main/java/com/crimson/avatar/player/Player.java
+++ src/main/java/com/crimson/avatar/player/Player.java
@@ -1,20 +1,36 @@
 package com.crimson.avatar.player;
 
 import com.crimson.avatar.Avatar;
-import com.crimson.avatar.AvatarNetwork;
 import com.crimson.bakuretsu.models.avatar.user.User;
 import com.crimson.interfaces.IDispatchable;
+import com.crimson.network.encoder.NetworkEncoder;
 import io.netty.channel.Channel;
 import net.sf.json.JSONObject;
 
 public class Player extends Avatar<User> implements IDispatchable {
 
-	private final AvatarNetwork network;
-	public final User data;
+	private final int networkId;
+	private final String name;
+	private final Channel channel;
+	private final User data;
 
 	public Player(int networkId, String name, Channel channel, User data) {
-		this.network = new AvatarNetwork(networkId, name, channel);
+		this.networkId = networkId;
+		this.name = name;
+		this.channel = channel;
 		this.data = data;
+	}
+
+	public int networkId() {
+		return networkId;
+	}
+
+	public String name() {
+		return name;
+	}
+
+	public Channel channel() {
+		return channel;
 	}
 
 	@Override
@@ -22,13 +38,9 @@ public class Player extends Avatar<User> implements IDispatchable {
 		return this.data;
 	}
 
-	public AvatarNetwork network() {
-		return this.network;
+	@Override
+	public void dispatch(JSONObject params) {
+		NetworkEncoder.dispatch(params, channel());
 	}
-
-    @Override
-    public void dispatch(JSONObject params) {
-
-    }
 
 }
